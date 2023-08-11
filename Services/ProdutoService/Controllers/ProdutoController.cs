32174e75cc5a2a799b1cc7c6ebce9f0a44e8a057
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2023-08-11 09:15:24 -0300

    Salvando desenvolvimento

---
+++ Services/ProdutoService/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProdutoService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        [HttpGet]
+        public bool Teste() { return true; }
+    }
+}
